1605a2c7cd9052a571d0f5085672a09f065df82d
alexander-gesinn <alexander@gesinn.de>
2025-02-04 15:11:55 +0100

    add further customization options

--- ckan/docker-entrypoint.d/00_configure_core.sh
+++ ckan/docker-entrypoint.d/00_configure_core.sh
@@ -6,6 +6,7 @@ ckan config-tool "$CKAN_INI" "WTF_CSRF_SSL_STRICT=${WTF_CSRF_SSL_STRICT:-true}"
 ## Branding
 ckan config-tool "$CKAN_INI" "ckan.site_title=${CKAN_SITE_TITLE:-CKAN}"
 ckan config-tool "$CKAN_INI" "ckan.site_logo=${CKAN_SITE_LOGO:-/base/images/ckan-logo.png}"
+ckan config-tool "$CKAN_INI" "ckan.site_custom_css=$CKAN_SITE_CUSTOM_CSS"
 ckan config-tool "$CKAN_INI" "ckan.theme=${CKAN_THEME:-css/main}"
 
 ## E-Mail
