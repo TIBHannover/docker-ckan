b124948b7100052949bedb84cabab7a164cbc294
alexander-gesinn <alexander@gesinn.de>
2025-04-07 09:38:51 +0200

    add (configure): CKAN_MAX_RESOURCE_SIZE

--- ckan/docker-entrypoint.d/00_configure_core.sh
+++ ckan/docker-entrypoint.d/00_configure_core.sh
@@ -5,10 +5,6 @@
 ## Core
 ckan config-tool "$CKAN_INI" "beaker.session.key=${SESSION_COOKIE_NAME:-ckan}"
 ckan config-tool "$CKAN_INI" "WTF_CSRF_SSL_STRICT=${WTF_CSRF_SSL_STRICT:-true}"
-# The storage_path is a container internal from the official ckan docker image and should not be changed
-# We write it here to ckan.ini to ensure it has the correct value
-ckan config-tool "$CKAN_INI" "ckan.storage_path=/var/lib/ckan"
-
 ckan config-tool "$CKAN_INI" "ckan.auth.create_user_via_web=${CKAN_AUTH_CREATE_USER_VIA_WEB:-false}"
 
 ## Branding
@@ -25,3 +21,9 @@ ckan config-tool "$CKAN_INI" "smtp.password=$CKAN_SMTP_PASSWORD"
 ckan config-tool "$CKAN_INI" "smtp.mail_from=$CKAN_SMTP_MAIL_FROM"
 ckan config-tool "$CKAN_INI" "smtp.reply_to=$CKAN_SMTP_REPLY_TO"
 ckan config-tool "$CKAN_INI" "ckan.activity_streams_email_notifications=$CKAN_ACTIVITY_STREAMS_EMAIL_NOTIFICATION"
+
+## Storage
+# The storage_path is a container internal from the official ckan docker image and should not be changed
+# We write it here to ckan.ini to ensure it has the correct value
+ckan config-tool "$CKAN_INI" "ckan.storage_path=/var/lib/ckan"
+ckan config-tool "$CKAN_INI" "ckan.max_resource_size=$CKAN_MAX_RESOURCE_SIZE"
